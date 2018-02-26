8d11f1b8a95a6fb7e960fc597fc615b778efd4ec
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-02-26 12:12:06 +0000

    First simulation (#1)
    Request token and check its validity multiple times.
    
    Values for URL, service name and secret are read from configuration 
    file.
    
    Parameters for simulation are subject to change.
    
    Client checks continuously obtained JWT for 2 minutes with 200
    millisecond pause.
    
    Maximum number of users is generously set to 400.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Service to Service Authorisation Performance Tests
+
+Generating and checking auth-tokens.
+
+
+## Prerequisites
+
+* Java Runtime Environment 8
+
+
+## Running
+```bash
+./gradlew gatlingRun
+```
+
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ src/gatling/scala/SingleLeaseMultipleChecks.scala
@@ -0,0 +1,46 @@
+import com.typesafe.config.{Config, ConfigFactory}
+import com.warrenstrange.googleauth.GoogleAuthenticator
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+class SingleLeaseMultipleChecks extends Simulation {
+  val config: Config = ConfigFactory.load()
+
+  val authenticator: GoogleAuthenticator = new GoogleAuthenticator()
+
+  val requestJwt = exec(
+    http("Lease token")
+      .post("/lease")
+      .formParam("microservice", config.getString("service.name"))
+      .formParam("oneTimePassword", "${otp}")
+      .check(bodyString.saveAs("jwt"))
+  )
+
+  val checkJwt = exec(
+    http("Check token")
+      .get("/details")
+      .header("Authorization", "Bearer ${jwt}")
+      .check(substring(config.getString("service.name")))
+  )
+
+  val otpFeeder = Iterator.continually(Map("otp" -> authenticator.getTotpPassword(config.getString("service.pass"))))
+
+  setUp(
+    scenario("Testing")
+      .feed(otpFeeder)
+      .exec(requestJwt)
+      .during(2 minutes) {
+        exec(checkJwt)
+          .pause(200 millis)
+      }
+      .inject(
+        rampUsers(400) over (1 minute)
+      )
+  )
+    .protocols(
+      http
+        .baseURL(config.getString("baseUrl"))
+    )
+}
